d8e5a46cf88f29cc3270d73b61c127ed4643b944
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-07-23 22:12:56 +0530

    guessing game

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-// script.js
-fetch('mytext.txt')
-  .then(response => response.text())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error fetching the text file:', error));
+let input = document.getElementById('input');
+let btn = document.getElementById('btn');
+let wrong = document.querySelector('.wrong'); // Corrected to use querySelector for class selection
+let guesses = document.getElementById('guesses');
+
+let answer = Math.floor(Math.random() * 100) + 1;
+
+let numberGuesses = 0;
+
+btn.addEventListener("click", () => {
+  guessNumber(); // Corrected the function name
+});
+
+function guessNumber() { // Corrected the function name
+  if (input.value < 1 || input.value > 100 || isNaN(input.value)) {
+    wrong.innerHTML = "Enter a number between 1 to 100";
+  } else {
+    numberGuesses++;
+    guesses.innerHTML = "No. of Guess: " + numberGuesses;
+
+    if (input.value > answer) {
+      wrong.innerHTML = "Your guess is too high!";
+      input.value = "";
+    } else if (input.value < answer) {
+      wrong.innerHTML = "Your guess is too low!";
+      input.value = "";
+    } else {
+      wrong.innerHTML = "Congratulations! You guessed the correct answer!";
+      setTimeout(() => {
+        resetGame();
+      }, 5000);
+    }
+  }
+}
+
+function resetGame() {
+  numberGuesses = 0;
+  answer = Math.floor(Math.random() * 100) + 1;
+  input.value = "";
+  guesses.innerHTML = "No. of Guess: 0";
+  wrong.innerHTML = "";
+}
